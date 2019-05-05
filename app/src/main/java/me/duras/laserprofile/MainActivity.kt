887788c88e01b46561d27ef95bbbd4fc3f527b54
Jakub Ďuraš <jakub@duras.me>
2019-05-06 01:49:18 +0200

    Add fragments navigation

--- app/src/main/java/me/duras/laserprofile/MainActivity.kt
+++ app/src/main/java/me/duras/laserprofile/MainActivity.kt
@@ -3,32 +3,23 @@ package me.duras.laserprofile
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import kotlinx.android.synthetic.main.activity_main.*
+import me.duras.laserprofile.dummy.DummyContent
 
-class MainActivity : AppCompatActivity() {
-
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
+class MainActivity : AppCompatActivity(), GamesFragment.OnListFragmentInteractionListener, RankingsFragment.OnListFragmentInteractionListener {
+    override fun onListFragmentInteraction(item: DummyContent.DummyItem?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        NavigationUI.setupWithNavController(navigation, navController)
     }
 }
---
+++ app/src/main/java/me/duras/laserprofile/ProfileViewModel.kt
@@ -0,0 +1,7 @@
+package me.duras.laserprofile
+
+import androidx.lifecycle.ViewModel;
+
+class ProfileViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
